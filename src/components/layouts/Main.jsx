ec03822a60e5597f9c8b051bfd3c4cb7a0cc14c5
bradyshehu <shehu.brady@gmail.com>
2025-05-27 15:10:34 +0200

    finish form

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [shoppingList, setShoppingList] = useState([
+    "Pane",
+    "Pasta",
+    "Biscotti",
+    "Latte",
+  ]);
+
+  const [newShoppingItem, setNewShoppingItem] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newShoppingList = [...shoppingList, newShoppingItem];
+    setShoppingList(newShoppingList);
+    setNewShoppingItem("");
+  };
+
   return (
     <main>
-      <h1>App React</h1>
+      <div className="container">
+        <form onSubmit={handleFormSubmit} className="dflex gap-2">
+          <input
+            type="text"
+            className="form-control"
+            value={newShoppingItem}
+            onChange={(event) => setNewShoppingItem(event.target.value)}
+          />
+        </form>
+        <ul>
+          {shoppingList.map((shoppingItem, index) => (
+            <li key={index}>{shoppingItem}</li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
